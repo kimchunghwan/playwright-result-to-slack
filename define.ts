89e467bca4726f96a256cbf37c7591040f8c0252
kimchunghwan <keii2k84@gmail.com>
2025-01-11 13:34:08 +0900

    Update define.ts

--- define.ts
+++ define.ts
@@ -1,5 +1,5 @@
 export const FINVIZ_SYMBOLS = ["TSLA", "PLTR", "QQQ", "SOXX", "SCHD", "IVV", "TLT", "GOOGL"];
-export const GROWTH_SYMBOLS = ["TSLA", "PLTR", "QQQ", "SOXX"];
+export const GROWTH_SYMBOLS = ["TSLA", "PLTR", "QQQ", "SOXX", "RXRX"];
 export const finvizURL = (symbol: string) => {
   if (GROWTH_SYMBOLS.includes(symbol)) {
     return `https://finviz.com/quote.ashx?t=${symbol}&p=d`;
