f9fe08d8e133530977057e50d8a39f1fcb527fee
kimchunghwan <keii2k84@gmail.com>
2025-03-05 18:21:49 +0900

    Revert "Add Playwright test for US government bond yield and export test resu…"

--- tests/bond.yield.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from "@playwright/test";
-import { testResultPath } from "./stock.spec";
-
-test('test', async ({ page }) => {
-  await page.goto('https://ko.tradingeconomics.com/united-states/government-bond-yield');
-  await page.getByText('5Y', { exact: true }).click();
-  page.locator('.highcharts-background').screenshot({ path: `./${testResultPath}/US_10Y.png` });
-});
