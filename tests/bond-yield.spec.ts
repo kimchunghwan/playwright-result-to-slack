35eff65109bc521fa97950b02ed9ca1538d63720
chunghwan kim <keii2k84@gmail.com>
2025-03-05 18:33:37 +0900

    test

--- tests/bond-yield.spec.ts
+++ tests/bond-yield.spec.ts
@@ -2,8 +2,10 @@ import { test, expect } from "@playwright/test";
 
 const testResultPath = "test-results";
 test('test', async ({ page }) => {
+
   await page.goto('https://ko.tradingeconomics.com/united-states/government-bond-yield');
+  await page.setViewportSize({ width: 1920, height: 1920 });  
   await page.getByText('5Y', { exact: true }).click();
   await page.waitForTimeout(1000);
-  page.locator('.highcharts-background').screenshot({ path: `./${testResultPath}/US_10Y.png` });
+  await page.locator('.highcharts-background').screenshot({ path: `./${testResultPath}/US_10Y.png` });
 });
