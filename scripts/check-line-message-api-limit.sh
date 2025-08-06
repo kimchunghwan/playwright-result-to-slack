e7616e408fd9cc2cbf2e016c502c2d58e13f66c7
kimchunghwan <keii2k84@gmail.com>
2025-08-06 10:06:31 +0900

    Add GitHub Actions workflow to check LINE messaging API usage and notify via Slack
    Refactor access token handling to use environment variables for LINE and Slack tokens

---
+++ scripts/check-line-message-api-limit.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+LINE_ACCESS_TOKEN="${LINE_ACCESS_TOKEN_TERASS:-}"
+SLACK_BOT_TOKEN="${SLACK_BOT_TOKEN_TERASS:-}"
+NOTIFICATION_PERCENTAGE=80 # Notification threshold percentage
+SLACK_CHANNEL_ID="C057FBXAGF4" # todo check alert channel
+
+if [ -z "$LINE_ACCESS_TOKEN" ]; then
+  echo "Error: LINE_ACCESS_TOKEN environment variable is not set"
+  exit 1
+fi
+
+if [ -z "$SLACK_BOT_TOKEN" ]; then
+  echo "Error: SLACK_BOT_TOKEN environment variable is not set"
+  exit 1
+fi
+
+totalUsage=$(curl -s -X GET https://api.line.me/v2/bot/message/quota/consumption \
+-H "Authorization: Bearer $LINE_ACCESS_TOKEN" | jq '.totalUsage')
+
+value=$(curl -s -X GET https://api.line.me/v2/bot/message/quota \
+-H "Authorization: Bearer $LINE_ACCESS_TOKEN" | jq '.value')
+
+echo "Total Usage: $totalUsage"
+echo "Quota Value: $value"
+
+if [[ $value -ne 0 ]]; then
+  percentage=$(echo "scale=2; ($totalUsage / $value) * 100" | bc)
+  echo "Usage: $percentage%"
+  
+  if (( $(echo "$percentage > $NOTIFICATION_PERCENTAGE" | bc -l) )); then
+     curl -X POST \
+    -H "Authorization: Bearer $SLACK_BOT_TOKEN" \
+    -H 'Content-type: application/json; charset=utf-8' \
+    --data-binary \
+    "{\"channel\":\"${SLACK_CHANNEL_ID}\",\"text\":\"警告: LINE Message APIの使用率が${percentage}%に達しました！詳細はこちらで確認してください: https://manager.line.biz/account/@560whmnl/insight/message\"}" \
+    https://slack.com/api/chat.postMessage
+  fi
+else
+  echo "Quota value is zero, cannot calculate percentage."
+fi
